908b53e34fef1d8f7e88ec351932b99402204de0
Benjamin Adeyele <benadeyele@gmail.com>
2024-03-09 22:34:48 +0100

    deploy ec2

--- my_ec2/variables.tf
+++ my_ec2/variables.tf
@@ -6,9 +6,9 @@ variable "key_pair_name" {}
 variable "instance_type" {}
 variable "map_public_ip_on_launch_true_false" {}
 variable "region" {}
-variable "AWS_ACCESS_KEY_ID" {
+# variable "AWS_ACCESS_KEY_ID" {
   
-}
-variable "AWS_SECRET_ACCESS_KEY" {
+# }
+# variable "AWS_SECRET_ACCESS_KEY" {
   
-}
+# }
